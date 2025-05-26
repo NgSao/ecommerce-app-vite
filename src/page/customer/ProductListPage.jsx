04bfbbbd81597fbb05023eed39489d9597c2022c
NgSao <nguyensaovn2019@gmail.com>
2025-05-26 22:31:57 +0700

    updated

--- src/page/customer/ProductListPage.jsx
+++ src/page/customer/ProductListPage.jsx
@@ -288,7 +288,7 @@ const ProductListPage = () => {
                     ))}
                 </div>
                 <div
-                    className={`p-4 grid gap-4 ${viewMode === "grid"
+                    className={`p-5 grid gap-4 ${viewMode === "grid"
                         ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
                         : "grid-cols-1"
                         }`}
